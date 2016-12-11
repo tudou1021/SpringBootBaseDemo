49c0d1dc2436cd26793f6cc5df6754b0a1f67f28
tudoubig <86412423@qq.com>
2016-12-11 11:50:50 +0800

    添加redis缓存管理类及redis工具类

---
+++ src/main/java/com/demo/config/RedisCacheConfig.java
@@ -0,0 +1,90 @@
+package com.demo.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * redis缓存管理类
+ * User: tudoubig
+ * Date: 2016/12/11
+ * Time: 11:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+@CacheConfig
+@EnableCaching
+public class RedisCacheConfig extends CachingConfigurerSupport {
+
+    /**
+     * 生成key的策略
+     * @return
+     */
+    @Override
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    /**
+     * 管理缓存
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public CacheManager cacheManager(RedisTemplate redisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+        //设置缓存过期时间
+        // cacheManager.setDefaultExpiration(60);//秒
+        //设置value的过期时间
+        Map<String,Long> map=new HashMap();
+        map.put("test",60L);
+        cacheManager.setExpires(map);
+        return cacheManager;
+    }
+
+    /**
+     * RedisTemplate配置
+     * @param factory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate template = new StringRedisTemplate(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
--- src/test/java/com/demo/ApplicationTests.java
+++ src/test/java/com/demo/ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.demo;
 
 import com.demo.controller.UserController;
+import com.demo.utils.RedisUtil;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
@@ -19,22 +21,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MockServletContext.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class ApplicationTests {
 
-	private MockMvc mvc;
-
-	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
-	}
+	@Autowired
+	private RedisUtil redisUtil;
 
 	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello World")));
+	public void redisTest(){
+		String key="tudou";
+		boolean flag=redisUtil.set(key,"18600198020");
+		System.out.println("执行set方法："+flag);
+		String mobile= (String) redisUtil.get(key);
+		System.out.println("mobile:"+mobile);
+		flag=redisUtil.set(key,"123123123");
+		System.out.println("执行reset："+flag);
+		String value= (String) redisUtil.get(key);
+		System.out.println("value:"+value);
 	}
+
 
 }
